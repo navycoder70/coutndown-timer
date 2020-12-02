d9a2618391c1735776aa2fe38840db57b1ffd06b
mehmet <mehmetozlu0@gmail.com>
2020-12-03 00:33:41 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl =  document.getElementById('mins');
+const secondsEl  = document.getElementById('seconds');
+
+const newYears = '29 Oct 2023';
+
+function countdown(){
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+
+    const tseconds = (newYearsDate - currentDate) / 1000;
+
+    const days =  Math.floor(tseconds / 3600 / 24);
+    const hours = Math.floor(tseconds / 3600) % 24;
+    const mins = Math.floor(tseconds / 60) % 60;
+    const seconds = Math.floor(tseconds) % 60;
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
+}
+
+
+function formatTime(time){
+    return time < 10 ?(`0${time}`)  : time;
+}
+countdown();
+
+setInterval(countdown, 1000);
+
